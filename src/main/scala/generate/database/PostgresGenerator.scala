bef1a2dbf138e0d1600c4417f81cebee9d686dc7
Lewis Bell <lewisbell999@gmail.com>
2020-01-30 14:42:35 +0000

    [SQL-Gen] Fix extra newline

--- src/main/scala/generate/database/PostgresGenerator.scala
+++ src/main/scala/generate/database/PostgresGenerator.scala
@@ -28,7 +28,7 @@ object PostgresGenerator extends DatabaseGenerator {
       case Create(tableName, columns) =>
         sb.append(s"CREATE TABLE $tableName ")
         val stringColumns = columns map parseColumn
-        sb.append(wrap(stringColumns.mkString("", ",\n", ""), left = "(\n", right = ")\n"))
+        sb.append(wrap(stringColumns.mkString("", ",\n", "\n"), left = "(\n"))
         sb.append(";\n")
     }
     sb.mkString
