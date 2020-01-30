4db63a0b520afeb424179667c9cf6525cfa2c505
Lewis Bell <lewisbell999@gmail.com>
2020-01-30 12:53:57 +0000

    [SQL-Gen] Reword create test

--- src/test/scala/generate/database/PostgresGeneratorTest.scala
+++ src/test/scala/generate/database/PostgresGeneratorTest.scala
@@ -2,7 +2,7 @@ package generate.database
 import org.scalatest.{FlatSpec, Matchers}
 
 class PostgresGeneratorTest extends FlatSpec with Matchers {
-    "PostgresGenerator" should "output correctly" in {
+    "PostgresGenerator" should "generate correct CREATE statements" in {
       PostgresGenerator.generate(TestData.create_Statement) shouldBe TestData.create_String
     }
 }
