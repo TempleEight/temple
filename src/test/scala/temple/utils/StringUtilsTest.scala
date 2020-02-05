2be577030906f3883ffdaebb608c3ff6930b1e4b
Charlie Harding <charlie_harding@icloud.com>
2020-02-05 18:15:10 +0000

    [Test-utils] Test String Utils

---
+++ src/test/scala/temple/utils/StringUtilsTest.scala
@@ -0,0 +1,22 @@
+package temple.utils
+
+import org.scalatest.{FlatSpec, Matchers}
+import StringUtils.indent
+
+class StringUtilsTest extends FlatSpec with Matchers {
+  "indent" should "add spaces to an empty string" in {
+    indent("") shouldEqual "  "
+  }
+  "indent" should "add n spaces to an empty string" in {
+    indent("", 1) shouldEqual " "
+    indent("", 3) shouldEqual "   "
+  }
+  "indent" should "add spaces to a single line" in {
+    indent("abcd") shouldEqual "  abcd"
+    indent("efgh", 4) shouldEqual "    efgh"
+  }
+  "indent" should "add spaces on each line" in {
+    indent("abcd\nefg", 1) shouldEqual " abcd\n efg"
+    indent("abcd\nefg\n", 1) shouldEqual " abcd\n efg\n "
+  }
+}
