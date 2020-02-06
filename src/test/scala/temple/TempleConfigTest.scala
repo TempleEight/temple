df4528456ea6a375ec1ffd39597e009752cdc1cc
Charlie Harding <charlie_harding@icloud.com>
2020-02-06 12:27:00 +0000

    [List to Seq] Update List to Seq in tests
    The Seq constructor can return an array-based structure as well as a list,
    which allows constant-time access

--- src/test/scala/temple/TempleConfigTest.scala
+++ src/test/scala/temple/TempleConfigTest.scala
@@ -5,25 +5,25 @@ import org.rogach.scallop.ScallopOption
 
 class TempleConfigTest extends FlatSpec with Matchers {
   "Generate" should "not correctly parse without a filename" in {
-    a[RuntimeException] should be thrownBy new TempleConfig(List("generate"))
+    a[RuntimeException] should be thrownBy new TempleConfig(Seq("generate"))
   }
 
   "Generate" should "correctly parse with a filename" in {
-    val config = new TempleConfig(List("generate", "foo.temple"))
+    val config = new TempleConfig(Seq("generate", "foo.temple"))
     config.subcommand shouldBe Some(config.Generate)
     config.Generate.filename.toOption shouldBe Some("foo.temple")
   }
 
   "Empty arguments" should "have no sub commands" in {
-    val config = new TempleConfig(List())
+    val config = new TempleConfig(Seq())
     config.subcommand shouldBe None
   }
 
   "Unknown subcommand" should "throw an exception" in {
-    an[IllegalArgumentException] should be thrownBy new TempleConfig(List("foobarbaz"))
+    an[IllegalArgumentException] should be thrownBy new TempleConfig(Seq("foobarbaz"))
   }
 
   "Unknown flag" should "throw an exception" in {
-    an[IllegalArgumentException] should be thrownBy new TempleConfig(List("-z"))
+    an[IllegalArgumentException] should be thrownBy new TempleConfig(Seq("-z"))
   }
 }
