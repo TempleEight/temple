426e173a70acedd92b415408ca02f2e196b13e46
Charlie Harding <charlie_harding@icloud.com>
2020-02-10 23:02:12 +0000

    [Syntax refactor] Use type alias for Syntax parser

--- src/main/scala/temple/DSL/DSLProcessor.scala
+++ src/main/scala/temple/DSL/DSLProcessor.scala
@@ -1,12 +1,11 @@
 package temple.DSL
 
-import temple.DSL.syntax.DSLRootItem
 import temple.DSL.parser.DSLParser
 
 object DSLProcessor extends DSLParser {
 
   /** Show an error message for a failed parse, with the line and number printed */
-  protected def printError(input: Input): String = {
+  private def printError(input: Input): String = {
     val lineNo = input.pos.line.toString
     lineNo + " | " + input.source.toString.split("\n")(input.pos.line - 1) + "\n" +
     (" " * (input.pos.column + 2 + lineNo.length)) + "^"
@@ -17,7 +16,7 @@ object DSLProcessor extends DSLParser {
     * @param contents the contents of a Templefile as a string
     * @return Right of the the parsed list of root elements, or Left of a string representing the error
     */
-  def parse(contents: String): Either[String, Seq[DSLRootItem]] =
+  def parse(contents: String): Either[String, syntax.Templefile] =
     parseAll(templefile, contents) match {
       case Success(result, _)    => Right(result)
       case NoSuccess(str, input) => Left(str + '\n' + printError(input))
