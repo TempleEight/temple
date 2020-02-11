dd1505c9774b6e1c2b368c350497ab16f08b710e
Charlie Harding <charlie_harding@icloud.com>
2020-02-11 14:42:44 +0000

    [Semantics] Revert TODO of possible test

--- src/test/scala/temple/DSL/parser/DSLParserTest.scala
+++ src/test/scala/temple/DSL/parser/DSLParserTest.scala
@@ -59,6 +59,4 @@ class DSLParserTest extends FlatSpec with Matchers {
     val reParsedResult = DSLProcessor.parse(reSourced).fromEither(msg => fail(s"second parse failed, $msg"))
     parseResult shouldBe reParsedResult
   }
-
-  // TODO: add: two things parse to the same result
 }
