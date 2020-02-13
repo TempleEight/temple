2bde77ce4ff0f2ac53ba4428197819852d57e60b
Jay Lees <jay.lees@me.com>
2020-02-13 10:03:21 +0000

    [CLI] Use nio library for file io

--- src/main/scala/temple/utils/FileUtils.scala
+++ src/main/scala/temple/utils/FileUtils.scala
@@ -1,9 +1,7 @@
 package temple.utils
 
-import java.io.{File, PrintWriter}
+import java.nio.charset.Charset
 import java.nio.file.{Files, Paths}
-
-import scala.io.Source
 
 /** Helper functions useful for manipulating files */
 object FileUtils {
@@ -13,15 +11,15 @@ object FileUtils {
 
   /** Write a string to file */
   def writeToFile(filename: String, s: String): Unit = {
-    val writer = new PrintWriter(new File(filename))
+    val path   = Paths.get(filename)
+    val writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"))
     try writer.write(s)
     finally writer.close()
   }
 
   /** Read a string from a file */
   def readFile(filename: String): String = {
-    val file = Source.fromFile(name = filename)
-    try file.mkString
-    finally file.close()
+    val path = Paths.get(filename)
+    Files.readString(path)
   }
 }
