7ee6d1c2f70d2937ef06114f444e18f5e69847d8
Jay Lees <jay.lees@me.com>
2020-02-13 18:36:33 +0000

    [test] Use whitespace consistently and update file util tests

--- src/test/scala/temple/generate/database/PostgresGeneratorTest.scala
+++ src/test/scala/temple/generate/database/PostgresGeneratorTest.scala
@@ -12,49 +12,64 @@ class PostgresGeneratorTest extends FlatSpec with Matchers {
   it should "generate correct CREATE statements" in {
     PostgresGenerator.generate(TestData.createStatement) shouldBe TestData.postgresCreateString
   }
+
   it should "generate correct SELECT statements" in {
     PostgresGenerator.generate(TestData.readStatement) shouldBe TestData.postgresSelectString
   }
+
   it should "handle column constraints correctly" in {
     PostgresGenerator.generate(TestData.createStatementWithConstraints) shouldBe TestData.postgresCreateStringWithConstraints
   }
+
   it should "handle selects with WHERE correctly" in {
     PostgresGenerator.generate(TestData.readStatementWithWhere) shouldBe TestData.postgresSelectStringWithWhere
   }
+
   it should "generate correct INSERT statements" in {
     PostgresGenerator.generate(TestData.insertStatement) shouldBe TestData.postgresInsertString
   }
+
   it should "generate correct INSERT statements with question marks" in {
     val questionMarkContext: PostgresContext = PostgresContext(PreparedType.QuestionMarks)
     PostgresGenerator.generate(TestData.insertStatement)(questionMarkContext) shouldBe TestData.postgresInsertStringWithQuestionMarks
   }
+
   it should "handle conjunctions in WHERE correctly" in {
     PostgresGenerator.generate(TestData.readStatementWithWhereConjunction) shouldBe TestData.postgresSelectStringWithWhereConjunction
   }
+
   it should "handle disjunctions in WHERE correctly" in {
     PostgresGenerator.generate(TestData.readStatementWithWhereDisjunction) shouldBe TestData.postgresSelectStringWithWhereDisjunction
   }
+
   it should "handle inverse in WHERE correctly" in {
     PostgresGenerator.generate(TestData.readStatementWithWhereInverse) shouldBe TestData.postgresSelectStringWithWhereInverse
   }
+
   it should "generate correct UPDATE statements" in {
     PostgresGenerator.generate(TestData.updateStatement) shouldBe TestData.postgresUpdateString
   }
+
   it should "handle updates with WHERE correctly" in {
     PostgresGenerator.generate(TestData.updateStatementWithWhere) shouldBe TestData.postgresUpdateStringWithWhere
   }
+
   it should "generate correct DELETE statements" in {
     PostgresGenerator.generate(TestData.deleteStatement) shouldBe TestData.postgresDeleteString
   }
+
   it should "handle deletes with WHERE correctly" in {
     PostgresGenerator.generate(TestData.deleteStatementWithWhere) shouldBe TestData.postgresDeleteStringWithWhere
   }
+
   it should "handle DROP TABLEs correctly" in {
     PostgresGenerator.generate(TestData.dropStatement) shouldBe TestData.postgresDropString
   }
+
   it should "handle DROP TABLE IF EXISTS correctly" in {
     PostgresGenerator.generate(TestData.dropStatementIfExists) shouldBe TestData.postgresDropStringIfExists
   }
+
   it should "handle complex SELECT statements" in {
     PostgresGenerator.generate(TestData.readStatementComplex) shouldBe TestData.postgresSelectStringComplex
   }
--- src/test/scala/temple/utils/StringUtilsTest.scala
+++ src/test/scala/temple/utils/StringUtilsTest.scala
@@ -5,17 +5,21 @@ import temple.utils.StringUtils.indent
 
 class StringUtilsTest extends FlatSpec with Matchers {
   behavior of "indent"
+
   it should "add spaces to an empty string" in {
     indent("") shouldEqual "  "
   }
+
   it should "add n spaces to an empty string" in {
     indent("", 1) shouldEqual " "
     indent("", 3) shouldEqual "   "
   }
+
   it should "add spaces to a single line" in {
     indent("abcd") shouldEqual "  abcd"
     indent("efgh", 4) shouldEqual "    efgh"
   }
+
   it should "add spaces on each line" in {
     indent("abcd\nefg", 1) shouldEqual " abcd\n efg"
     indent("abcd\nefg\n", 1) shouldEqual " abcd\n efg\n "
