18cbf3bf8172a792bea90bf007bbb59412c3f110
Jay Lees <jay.lees@me.com>
2020-02-13 10:05:20 +0000

    [CLI] Repeat create directory in test and check no exception is thrown

--- src/test/scala/temple/utils/FileUtilsTest.scala
+++ src/test/scala/temple/utils/FileUtilsTest.scala
@@ -7,6 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class FileUtilsTest extends FlatSpec with Matchers {
   "Creating a folder that already exists" should "not throw an exception" in {
     FileUtils.createDirectory("src")
+    noException should be thrownBy FileUtils.createDirectory("src")
   }
 
   "Creating a folder where a file with that name already exists" should "throw an exception" in {
