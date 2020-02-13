8aa135dbec59d9b13be0a309d66dd0374d8a37d0
Lewis Bell <lewisbell999@gmail.com>
2020-02-13 16:54:02 +0000

    [Docker-Gen] Remove unneeded string utils

--- src/main/scala/temple/utils/StringUtils.scala
+++ src/main/scala/temple/utils/StringUtils.scala
@@ -5,10 +5,4 @@ object StringUtils {
 
   /** Given a string, indent it by a given number of spaces */
   def indent(str: String, length: Int = 2): String = str.replaceAll("^|(?<=\n)", " " * length)
-
-  /** Given a string and a wrapping char, append the char at each end */
-  def wrap(str: String, char: Char = '"'): String = s"$char$str$char"
-
-  /** Given a string that may contain quotes, automatically escape them */
-  def escapeQuotes(str: String): String = str.split('"').mkString("\\\"")
 }
--- src/test/scala/temple/utils/StringUtilsTest.scala
+++ src/test/scala/temple/utils/StringUtilsTest.scala
@@ -1,7 +1,7 @@
 package temple.utils
 
 import org.scalatest.{FlatSpec, Matchers}
-import temple.utils.StringUtils.{escapeQuotes, indent, wrap}
+import temple.utils.StringUtils.indent
 
 class StringUtilsTest extends FlatSpec with Matchers {
   "indent" should "add spaces to an empty string" in {
@@ -18,22 +18,5 @@ class StringUtilsTest extends FlatSpec with Matchers {
   "indent" should "add spaces on each line" in {
     indent("abcd\nefg", 1) shouldEqual " abcd\n efg"
     indent("abcd\nefg\n", 1) shouldEqual " abcd\n efg\n "
-  }
-
-  "wrap" should "add quotes to an empty string" in {
-    wrap("") shouldEqual "\"\""
-  }
-  "wrap" should "add quotes to a single line string" in {
-    wrap("test") shouldEqual "\"test\""
-  }
-  "wrap" should "add a different char to a single line string" in {
-    wrap("test", '_') shouldEqual "_test_"
-  }
-
-  "escapeQuotes" should "do nothing to a string without quotes" in {
-    escapeQuotes("test") shouldEqual "test"
-  }
-  "escapeQuotes" should "escape quotes in a string with quotes" in {
-    escapeQuotes("""foo " bar""") shouldEqual """foo \" bar"""
   }
 }
