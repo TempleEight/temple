e52cbbece0cdb3328772c481ab9c6087240d3e0d
Lewis Bell <lewisbell999@gmail.com>
2020-02-13 16:51:46 +0000

    [Docker-Gen] refactor array strings to use JSON lib

--- src/main/scala/temple/generate/docker/DockerFileGenerator.scala
+++ src/main/scala/temple/generate/docker/DockerFileGenerator.scala
@@ -1,15 +1,16 @@
 package temple.generate.docker
 
+import io.circe.Printer
+import io.circe.syntax._
 import temple.generate.docker.ast.Statement._
 import temple.generate.docker.ast.{DockerfileRoot, Statement}
-import temple.utils.StringUtils
 
 /** Generator object for building Dockerfiles from the Dockerfile AST objects */
 object DockerFileGenerator {
 
   /** Given a sequence of strings, build them in to a string of form ["a", "b", "c"] */
   private def buildArrayString(strs: Seq[String]): String =
-    strs.map(StringUtils.escapeQuotes).map(StringUtils.wrap(_)).mkString("[", ", ", "]")
+    strs.asJson.printWith(Printer(dropNullValues = false, indent = "", arrayCommaRight = " "))
 
   /** Given a [[temple.generate.docker.ast.Statement]], generate a valid string */
   private def generateStatement(statement: Statement): String =
