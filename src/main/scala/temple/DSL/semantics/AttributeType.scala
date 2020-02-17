b60ded7ca2bff09ee8caec111a91f45f1da367c7
Charlie Harding <charlie_harding@icloud.com>
2020-02-17 11:13:55 +0000

    [Semantics] Default to zero if a minimum is not given but max is

--- src/main/scala/temple/DSL/semantics/AttributeType.scala
+++ src/main/scala/temple/DSL/semantics/AttributeType.scala
@@ -13,8 +13,31 @@ object AttributeType {
   case class BlobType(size: Option[Long] = None)                           extends AttributeType
   case class StringType(max: Option[Long] = None, min: Option[Int] = None) extends AttributeType
 
-  case class IntType(max: Option[Long] = None, min: Option[Long] = None, precision: Byte = 4) extends AttributeType
+  case class IntType private (
+    max: Option[Long] = None,
+    min: Option[Long] = None,
+    precision: Byte = IntType.defaultPrecision,
+  ) extends AttributeType
 
-  case class FloatType(max: Option[Double] = None, min: Option[Double] = None, precision: Byte = 8)
-      extends AttributeType
+  object IntType {
+    private val defaultPrecision: Byte = 4
+
+    def apply(max: Option[Long] = None, min: Option[Long] = None, precision: Byte = defaultPrecision): IntType =
+      if (min.isEmpty && max.fold(false)(_ > 0)) new IntType(max, Some(0), precision)
+      else new IntType(max, min, precision)
+  }
+
+  case class FloatType(
+    max: Option[Double] = None,
+    min: Option[Double] = None,
+    precision: Byte = FloatType.defaultPrecision,
+  ) extends AttributeType
+
+  object FloatType {
+    private val defaultPrecision: Byte = 8
+
+    def apply(max: Option[Double] = None, min: Option[Double] = None, precision: Byte = defaultPrecision): FloatType =
+      if (min.isEmpty && max.fold(false)(_ > 0)) new FloatType(max, Some(0), precision)
+      else new FloatType(max, min, precision)
+  }
 }
