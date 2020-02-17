42fc5480e7c169f2569eb7ea66b17ef1a43111dc
Jay Lees <jay.lees@me.com>
2020-02-17 15:23:31 +0000

    [test] Add NOT NULL constraint to e2e test data

--- src/e2e/scala/temple/SimpleEndToEndTestData.scala
+++ src/e2e/scala/temple/SimpleEndToEndTestData.scala
@@ -4,19 +4,19 @@ object SimpleEndToEndTestData {
 
   val createStatement: String =
     """CREATE TABLE User (
-      |  username TEXT,
-      |  email VARCHAR(40) CHECK (length(email) >= 5),
-      |  firstName TEXT,
-      |  lastName TEXT,
-      |  createdAt TIMESTAMPTZ,
-      |  numberOfDogs INT,
-      |  yeets BOOLEAN UNIQUE,
-      |  currentBankBalance REAL CHECK (currentBankBalance >= 0.0),
-      |  birthDate DATE,
-      |  breakfastTime TIME
+      |  username TEXT NOT NULL,
+      |  email VARCHAR(40) CHECK (length(email) >= 5) NOT NULL,
+      |  firstName TEXT NOT NULL,
+      |  lastName TEXT NOT NULL,
+      |  createdAt TIMESTAMPTZ NOT NULL,
+      |  numberOfDogs INT NOT NULL,
+      |  yeets BOOLEAN UNIQUE NOT NULL,
+      |  currentBankBalance REAL CHECK (currentBankBalance >= 0.0) NOT NULL,
+      |  birthDate DATE NOT NULL,
+      |  breakfastTime TIME NOT NULL
       |);
       |
       |CREATE TABLE Fred (
-      |  field TEXT
+      |  field TEXT NOT NULL
       |);""".stripMargin
 }
