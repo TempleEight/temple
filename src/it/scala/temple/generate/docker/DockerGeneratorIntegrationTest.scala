23d507ade222be6c00b469961905e545bde20b25
Jay Lees <jay.lees@me.com>
2020-02-20 18:01:50 +0000

    [test][docker] Add Dockerfile test

--- src/it/scala/temple/generate/docker/DockerGeneratorIntegrationTest.scala
+++ src/it/scala/temple/generate/docker/DockerGeneratorIntegrationTest.scala
@@ -2,6 +2,8 @@ package temple.generate.docker
 
 import org.scalatest.{BeforeAndAfter, Matchers}
 import temple.containers.HadolintSpec
+import temple.generate.docker.ast.DockerfileRoot
+import temple.generate.docker.ast.Statement._
 
 class DockerGeneratorIntegrationTest extends HadolintSpec with Matchers with BeforeAndAfter {
 
@@ -12,5 +14,24 @@ class DockerGeneratorIntegrationTest extends HadolintSpec with Matchers with Bef
 
   it should "fail when a latest tag is used" in {
     validate("FROM golang:latest\nEXPOSE 80") should include(Rules.useLatest)
+  }
+
+  behavior of "DockerfileGenerator"
+  it should "generate valid Dockerfile" in {
+    val content = DockerfileRoot(
+      From("golang", Some("123")),
+      Seq(
+        Run("echo", Seq("foo", "bar", "baz")),
+        Cmd("cat", Seq("foo", "bar", "baz")),
+        Env("alpha", "beta"),
+        Add("http://example.com/big.tar.xz", "/usr/src/things/"),
+        Copy("/tmp/foo", "/tmp/bar"),
+        Entrypoint("myapp", Seq("--build", "/path/to/random/file")),
+        Volume("myvolume"),
+        WorkDir("/etc/bin/lib/dev/rand"),
+        Expose(8080),
+      ),
+    )
+    validate(DockerfileGenerator.generate(content)) shouldBe empty
   }
 }
