8a961bd2709ff8f00a85716633743fc73d4b529b
Jay Lees <jay.lees@me.com>
2020-02-21 13:56:58 +0000

    [naming] Add reference for snake case regex

--- src/main/scala/temple/utils/StringUtils.scala
+++ src/main/scala/temple/utils/StringUtils.scala
@@ -7,6 +7,7 @@ object StringUtils {
   def indent(str: String, length: Int = 2): String = str.replaceAll("^|(?<=\n)", " " * length)
 
   /** Given a string, convert it to snake case */
+  // https://github.com/lift/framework/blob/f1b450db2dd6a22cf9ffe5576ec34c8e87118319/core/util/src/main/scala/net/liftweb/util/StringHelpers.scala#L91
   def snakeCase(str: String): String =
     str
       .replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2")
