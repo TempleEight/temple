8cc3f265e68e8c6c3599ca1f6e7ab15844b87431
Jay Lees <jay.lees@me.com>
2020-02-21 17:06:40 +0000

    [builder] Extract common test data

---
+++ src/test/scala/temple/builder/BuilderTestData.scala
@@ -0,0 +1,50 @@
+package temple.builder
+
+import temple.DSL.semantics.AttributeType.{BlobType, BoolType, DateTimeType, DateType, FloatType, IntType, StringType, TimeType}
+import temple.DSL.semantics.{Annotation, Attribute, ServiceBlock, StructBlock}
+
+import scala.collection.immutable.ListMap
+
+object BuilderTestData {
+
+  // TODO: This test doesn't include a `ForeignKey` attribute, since it is not yet supported
+  //  by the parser/semantic analysis. Once it is, please update these tests!
+  val sampleService: ServiceBlock = ServiceBlock(
+    ListMap(
+      "id"          -> Attribute(IntType()),
+      "bankBalance" -> Attribute(FloatType()),
+      "name"        -> Attribute(StringType()),
+      "isStudent"   -> Attribute(BoolType),
+      "dateOfBirth" -> Attribute(DateType),
+      "timeOfDay"   -> Attribute(TimeType),
+      "expiry"      -> Attribute(DateTimeType),
+      "image"       -> Attribute(BlobType()),
+    ),
+  )
+
+  val sampleComplexService: ServiceBlock = ServiceBlock(
+    ListMap(
+      "id"             -> Attribute(IntType(max = Some(100), min = Some(10), precision = 2)),
+      "anotherId"      -> Attribute(IntType(max = Some(100), min = Some(10))),
+      "yetAnotherId"   -> Attribute(IntType(max = Some(100), min = Some(10), precision = 8)),
+      "bankBalance"    -> Attribute(FloatType(max = Some(300), min = Some(0), precision = 4)),
+      "bigBankBalance" -> Attribute(FloatType(max = Some(123), min = Some(0))),
+      "name"           -> Attribute(StringType(max = None, min = Some(1))),
+      "initials"       -> Attribute(StringType(max = Some(5), min = Some(0))),
+      "isStudent"      -> Attribute(BoolType),
+      "dateOfBirth"    -> Attribute(DateType),
+      "timeOfDay"      -> Attribute(TimeType),
+      "expiry"         -> Attribute(DateTimeType),
+      "image"          -> Attribute(BlobType()),
+    ),
+    structs = ListMap(
+      "Test" -> StructBlock(
+        ListMap(
+          "favouriteColour" -> Attribute(StringType(), valueAnnotations = Set(Annotation.Unique)),
+          "bedTime"         -> Attribute(TimeType, valueAnnotations = Set(Annotation.Nullable)),
+          "favouriteNumber" -> Attribute(IntType(max = Some(10), min = Some(0))),
+        ),
+      ),
+    ),
+  )
+}
--- src/test/scala/temple/builder/DatabaseBuilderTest.scala
+++ src/test/scala/temple/builder/DatabaseBuilderTest.scala
@@ -6,12 +6,12 @@ class DatabaseBuilderTest extends FlatSpec with Matchers {
   behavior of "DatabaseBuilder"
 
   it should "correctly create a simple users table" in {
-    val createQuery = DatabaseBuilder.createServiceTables("temple_user", DatabaseBuilderTestData.sampleService)
+    val createQuery = DatabaseBuilder.createServiceTables("temple_user", BuilderTestData.sampleService)
     createQuery shouldBe DatabaseBuilderTestData.sampleServiceCreate
   }
 
   it should "correctly create a complex users table" in {
-    val createQuery = DatabaseBuilder.createServiceTables("temple_user", DatabaseBuilderTestData.sampleComplexService)
+    val createQuery = DatabaseBuilder.createServiceTables("temple_user", BuilderTestData.sampleComplexService)
     createQuery shouldBe DatabaseBuilderTestData.sampleComplexServiceCreate
   }
 }
