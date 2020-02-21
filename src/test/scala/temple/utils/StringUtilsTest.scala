103c3a1c6a1eef4bc6228e66e445eb2f6aa3d135
Jay Lees <jay.lees@me.com>
2020-02-21 14:40:23 +0000

    [naming] Add additional tests for snake case

--- src/test/scala/temple/utils/StringUtilsTest.scala
+++ src/test/scala/temple/utils/StringUtilsTest.scala
@@ -39,7 +39,19 @@ class StringUtilsTest extends FlatSpec with Matchers {
     snakeCase("someRANDOMCamelCaseCHARACTERS") shouldBe "some_random_camel_case_characters"
   }
 
-  it should "numbers" in {
+  it should "handle numbers" in {
     snakeCase("some1917Numbers") shouldBe "some1917_numbers"
+  }
+
+  it should "not change snake case strings" in {
+    snakeCase("already_has_snake_case") shouldBe "already_has_snake_case"
+  }
+
+  it should "deal with mixed naming" in {
+    snakeCase("foo_barBaz_boo") shouldBe "foo_bar_baz_boo"
+  }
+
+  it should "not remove leading underscores" in {
+    snakeCase("_fooBarBaz") shouldBe "_foo_bar_baz"
   }
 }
