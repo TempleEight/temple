8826085083af167dc7dcbef5de7cd649c44a953a
Will JV Smith <smithwjv@gmail.com>
2020-02-25 16:59:51 +0000

    [go-gen] Rename match/user _test.go to just match/user .go

--- src/test/scala/temple/generate/language/service/go/GoServiceGeneratorTestData.scala
+++ src/test/scala/temple/generate/language/service/go/GoServiceGeneratorTestData.scala
@@ -8,13 +8,13 @@ object GoServiceGeneratorTestData {
   val simpleServiceRoot: ServiceRoot = ServiceRoot("user", "github.com/TempleEight/spec-golang/user", Seq.empty)
 
   val simpleServiceFiles: Map[File, FileContent] = Map(
-    File("user", "user.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/user_test.go"),
+    File("user", "user.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/user.go"),
   )
 
   val simpleServiceRootWithComms: ServiceRoot =
     ServiceRoot("match", "github.com/TempleEight/spec-golang/match", Seq("user"))
 
   val simpleServiceFilesWithComms: Map[File, FileContent] = Map(
-    File("match", "match.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/match_test.go"),
+    File("match", "match.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/match.go"),
   )
 }
