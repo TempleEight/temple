9cd0ee583f51dbb277a3adc5a7c1b6ca835a709a
Charlie Harding <charlie_harding@icloud.com>
2020-02-26 19:18:07 +0100

    [go-gen] Refactor Go generation to use #97’s format

--- src/main/scala/temple/generate/language/service/go/GoServiceGenerator.scala
+++ src/main/scala/temple/generate/language/service/go/GoServiceGenerator.scala
@@ -2,52 +2,40 @@ package temple.generate.language.service.go
 
 import temple.generate.language.service.ServiceGenerator
 import temple.generate.language.service.adt._
-import temple.generate.utils.CodeTerm.CodeWrap
+import temple.generate.utils.CodeTerm
+import temple.generate.utils.CodeTerm.{CodeWrap, mkCode}
 import temple.utils.FileUtils
-import temple.utils.StringUtils.{doubleQuote, tabIndent}
-import scala.collection.mutable.ListBuffer
+import temple.utils.StringUtils.doubleQuote
+
+import scala.Option.when
 
 /** Implementation of [[ServiceGenerator]] for generating Go */
 object GoServiceGenerator extends ServiceGenerator {
 
-  private def generatePackage(packageName: String): String = s"package $packageName\n"
+  private def generatePackage(packageName: String): String = s"package $packageName"
 
   /** Given a service name, module name and whether the service uses inter-service communication, return the import
     * block */
   private def generateImports(serviceName: String, module: String, usesComms: Boolean): String = {
-    val sb = new StringBuilder
-    sb.append("import ")
+    val standardImports = Seq("encoding/json", "flag", "fmt", "log", "net/http").map(doubleQuote)
 
-    val standardImports = Seq("encoding/json", "flag", "fmt", "log", "net/http")
-      .map(doubleQuote)
-      .mkString("\n")
-
-    val customImportsBuffer: ListBuffer[String] = ListBuffer(
-      s"${serviceName}DAO ${doubleQuote(s"${module}/dao")}",
-    )
-    if (usesComms) {
-      customImportsBuffer += s"${serviceName}Comm ${doubleQuote(s"${module}/comm")}"
-    }
-    customImportsBuffer ++= Seq(
-      doubleQuote(s"${module}/util"),
+    val customImports = Seq[CodeTerm](
+      s"${serviceName}DAO ${doubleQuote(s"$module/dao")}",
+      when(usesComms) { s"${serviceName}Comm ${doubleQuote(s"$module/comm")}" },
+      doubleQuote(s"$module/util"),
       s"valid ${doubleQuote("github.com/asaskevich/govalidator")}",
       doubleQuote("github.com/gorilla/mux"),
     )
-    val customImports = customImportsBuffer.mkString("\n")
 
-    sb.append(CodeWrap.parens.tabbed(standardImports + "\n\n" + customImports))
-    sb.append("\n")
-    sb.toString
+    mkCode("import", CodeWrap.parens.tabbed(standardImports, "", customImports))
   }
 
   /** Given a service name and whether the service uses inter-service communication, return global statements */
-  private def generateGlobals(serviceName: String, usesComms: Boolean): String = {
-    val globals: ListBuffer[String] = ListBuffer(
+  private def generateGlobals(serviceName: String, usesComms: Boolean): String =
+    mkCode.lines(
       s"var dao ${serviceName}DAO.DAO",
+      when(usesComms) { s"var comm ${serviceName}Comm.Handler" },
     )
-    if (usesComms) globals += s"var comm ${serviceName}Comm.Handler"
-    globals.mkString("", "\n", "\n")
-  }
 
   /** Given a service name, whether the service uses inter-service communication, the endpoints desired and the port
     * number, generate the main function */
@@ -55,8 +43,8 @@ object GoServiceGenerator extends ServiceGenerator {
     val sb = new StringBuilder
     sb.append("func main() ")
 
-    val bodyBuffer: ListBuffer[String] = ListBuffer(
-      s"""configPtr := flag.String("config", "/etc/${serviceName}-service/config.json", "configuration filepath")""",
+    val body = mkCode.lines(
+      s"""configPtr := flag.String("config", "/etc/$serviceName-service/config.json", "configuration filepath")""",
       "flag.Parse()",
       "",
       "// Require all struct fields by default",
@@ -69,69 +57,68 @@ object GoServiceGenerator extends ServiceGenerator {
       "err = dao.Init(config)",
       "if err != nil " + CodeWrap.curly.tabbed("log.Fatal(err)"),
       "",
-    )
-
-    if (usesComms) {
-      bodyBuffer ++= Seq(
-        s"comm = ${serviceName}Comm.Handler{}",
-        "comm.Init(config)",
-        "",
-      )
-    }
-
-    bodyBuffer += "r := mux.NewRouter()"
-    if (endpoints.contains(Endpoint.ReadAll)) {
-      bodyBuffer ++= Seq(
-        "// Mux directs to first matching route, i.e. the order matters",
-        s"""r.HandleFunc("/${serviceName}/all", ${serviceName}ListHandler).Methods(http.MethodGet)""",
-      )
-    }
-    if (endpoints.contains(Endpoint.Create))
-      bodyBuffer += s"""r.HandleFunc("/${serviceName}", ${serviceName}CreateHandler).Methods(http.MethodPost)"""
-    if (endpoints.contains(Endpoint.Read))
-      bodyBuffer += s"""r.HandleFunc("/${serviceName}/{id}", ${serviceName}ReadHandler).Methods(http.MethodGet)"""
-    if (endpoints.contains(Endpoint.Update))
-      bodyBuffer += s"""r.HandleFunc("/${serviceName}/{id}", ${serviceName}UpdateHandler).Methods(http.MethodPut)"""
-    if (endpoints.contains(Endpoint.Delete))
-      bodyBuffer += s"""r.HandleFunc("/${serviceName}/{id}", ${serviceName}DeleteHandler).Methods(http.MethodDelete)"""
-
-    bodyBuffer ++= Seq(
+      when[CodeTerm](usesComms) {
+        Seq(
+          s"comm = ${serviceName}Comm.Handler{}",
+          "comm.Init(config)",
+          "",
+        )
+      },
+      "r := mux.NewRouter()",
+      when[CodeTerm](endpoints.contains(Endpoint.ReadAll)) {
+        Seq(
+          "// Mux directs to first matching route, i.e. the order matters",
+          s"""r.HandleFunc("/$serviceName/all", ${serviceName}ListHandler).Methods(http.MethodGet)""",
+        )
+      },
+      when(endpoints.contains(Endpoint.Create)) {
+        s"""r.HandleFunc("/$serviceName", ${serviceName}CreateHandler).Methods(http.MethodPost)"""
+      },
+      when(endpoints.contains(Endpoint.Read)) {
+        s"""r.HandleFunc("/$serviceName/{id}", ${serviceName}ReadHandler).Methods(http.MethodGet)"""
+      },
+      when(endpoints.contains(Endpoint.Update)) {
+        s"""r.HandleFunc("/$serviceName/{id}", ${serviceName}UpdateHandler).Methods(http.MethodPut)"""
+      },
+      when(endpoints.contains(Endpoint.Delete)) {
+        s"""r.HandleFunc("/$serviceName/{id}", ${serviceName}DeleteHandler).Methods(http.MethodDelete)"""
+      },
       "r.Use(jsonMiddleware)",
       "",
-      s"""log.Fatal(http.ListenAndServe(":${port}", r))""",
+      s"""log.Fatal(http.ListenAndServe(":$port", r))""",
     )
 
-    sb.append(CodeWrap.curly.tabbed(bodyBuffer.mkString("\n")))
-    sb.append("\n")
-    sb.toString
+    mkCode("func main()", CodeWrap.curly.tabbed(body))
   }
 
   private def generateJsonMiddleware(): String =
-    FileUtils.readFile("src/main/scala/temple/generate/language/service/go/genFiles/JsonMiddleware.go")
+    FileUtils.readFile("src/main/scala/temple/generate/language/service/go/genFiles/JsonMiddleware.go").stripLineEnd
 
   private def generateHandler(serviceName: String, endpoint: Endpoint): String =
-    s"func $serviceName${endpoint.verb}Handler(w http.ResponseWriter, r *http.Request) {}\n"
+    s"func $serviceName${endpoint.verb}Handler(w http.ResponseWriter, r *http.Request) {}"
 
   private def generateHandlers(serviceName: String, endpoints: Set[Endpoint]): String =
-    (for (endpoint <- Endpoint.values if endpoints.contains(endpoint))
-      yield generateHandler(serviceName, endpoint)).mkString("\n")
+    mkCode.doubleLines(
+      for (endpoint <- Endpoint.values if endpoints.contains(endpoint))
+        yield generateHandler(serviceName, endpoint),
+    )
 
   private def generateErrors(serviceName: String): String =
-    Seq(
+    mkCode.lines(
       "package dao",
       "",
       """import "fmt"""",
       "",
-      s"// Err${serviceName.capitalize}NotFound is returned when a ${serviceName} for the provided ID was not found",
+      s"// Err${serviceName.capitalize}NotFound is returned when a $serviceName for the provided ID was not found",
       s"type Err${serviceName.capitalize}NotFound int64",
       "",
       s"func (e Err${serviceName.capitalize}NotFound) Error() string ${CodeWrap.curly
-        .tabbed(s"""return fmt.Sprintf("${serviceName} not found with ID %d", e)""")}",
-    ).mkString("", "\n", "\n")
+        .tabbed(s"""return fmt.Sprintf("$serviceName not found with ID %d", e)""")}",
+    )
 
   override def generate(serviceRoot: ServiceRoot): Map[FileUtils.File, FileUtils.FileContent] = {
     val usesComms = serviceRoot.comms.nonEmpty
-    val serviceString = Seq(
+    val serviceString = mkCode.doubleLines(
       generatePackage("main"),
       generateImports(
         serviceRoot.name,
@@ -153,11 +140,11 @@ object GoServiceGenerator extends ServiceGenerator {
         serviceRoot.name,
         serviceRoot.endpoints,
       ),
-    ).mkString("\n")
+    )
     val errorsString = generateErrors(serviceRoot.name)
     Map(
-      FileUtils.File(serviceRoot.name, s"${serviceRoot.name}.go") -> serviceString,
-      FileUtils.File(s"${serviceRoot.name}/dao", "errors.go")     -> errorsString,
+      FileUtils.File(serviceRoot.name, s"${serviceRoot.name}.go") -> (serviceString + "\n"),
+      FileUtils.File(s"${serviceRoot.name}/dao", "errors.go")     -> (errorsString + "\n"),
     )
     /*
    * TODO:
