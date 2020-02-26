eec05e594223e4fe986b798008293ae54ea7c10c
Will JV Smith <smithwjv@gmail.com>
2020-02-26 17:32:21 +0000

    [go-gen] Improve endpoint enum

--- src/main/scala/temple/generate/language/service/adt/Endpoint.scala
+++ src/main/scala/temple/generate/language/service/adt/Endpoint.scala
@@ -1,28 +1,13 @@
 package temple.generate.language.service.adt
 
-sealed trait Endpoint
+sealed abstract class Endpoint(val verb: String)
 
 object Endpoint {
-
-  case object ReadAll extends Endpoint {
-    override def toString = "List"
-  }
-
-  case object Create extends Endpoint {
-    override def toString = "Create"
-  }
-
-  case object Read extends Endpoint {
-    override def toString = "Read"
-  }
-
-  case object Update extends Endpoint {
-    override def toString = "Update"
-  }
-
-  case object Delete extends Endpoint {
-    override def toString = "Delete"
-  }
+  case object ReadAll extends Endpoint("List")
+  case object Create  extends Endpoint("Create")
+  case object Read    extends Endpoint("Read")
+  case object Update  extends Endpoint("Update")
+  case object Delete  extends Endpoint("Delete")
 
   val values = Seq(ReadAll, Create, Read, Update, Delete)
 }
