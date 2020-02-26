2910fce660ba70b240025e930276e4f64677f467
Will JV Smith <smithwjv@gmail.com>
2020-02-26 12:25:36 +0000

    [go-gen] Add main generation

--- src/main/scala/temple/generate/language/service/go/GoServiceGenerator.scala
+++ src/main/scala/temple/generate/language/service/go/GoServiceGenerator.scala
@@ -13,7 +13,7 @@ object GoServiceGenerator extends ServiceGenerator {
   private def generatePackage(packageName: String): String = s"package $packageName\n\n"
 
   /** Given a service name, module name and whether the service uses inter-service communication, return the import block */
-  private def generateImports(serviceName: String, module: String, comms: Boolean): String = {
+  private def generateImports(serviceName: String, module: String, usesComms: Boolean): String = {
     val sb = new StringBuilder
     sb.append("import ")
 
@@ -22,7 +22,7 @@ object GoServiceGenerator extends ServiceGenerator {
       .mkString("\n")
 
     var customImportsBuffer: ListBuffer[String] = ListBuffer.empty
-    if (comms) {
+    if (usesComms) {
       customImportsBuffer += s"${serviceName}Comm ${doubleQuote(s"${module}/comm")}"
     }
     customImportsBuffer ++= Seq(
@@ -38,12 +38,87 @@ object GoServiceGenerator extends ServiceGenerator {
     sb.toString
   }
 
+  private def generateMain(serviceName: String, usesComms: Boolean, endpoints: Seq[Endpoint], port: Int): String = {
+    val sb = new StringBuilder
+    sb.append("func main() {\n")
+    sb.append(
+      tabIndent(
+        s"""configPtr := flag.String("config", "/etc/${serviceName}-service/config.json", "configuration filepath")""" + "\n",
+      ),
+    )
+    sb.append(tabIndent("flag.Parse()\n"))
+    sb.append("\n")
+    sb.append(tabIndent("// Require all struct fields by default\n"))
+    sb.append(tabIndent("valid.SetFieldsRequiredByDefault(true)\n"))
+    sb.append("\n")
+    sb.append(tabIndent("config, err := utils.GetConfig(*configPtr)\n"))
+    sb.append(tabIndent("if err != nil {\n"))
+    sb.append(tabIndent("log.Fatal(err)\n", 2))
+    sb.append(tabIndent("}\n"))
+    sb.append("\n")
+    sb.append(tabIndent(s"dao = ${serviceName}DAO.DAO{}\n"))
+    sb.append(tabIndent("err = dao.Init(config)\n"))
+    sb.append(tabIndent("if err != nil {\n"))
+    sb.append(tabIndent("log.Fatal(err)\n", 2))
+    sb.append(tabIndent("}\n"))
+    sb.append("\n")
+    if (usesComms) {
+      sb.append(tabIndent(s"comm = ${serviceName}Comm.Handler{}\n"))
+      sb.append(tabIndent("comm.Init(config)\n"))
+      sb.append("\n")
+    }
+    sb.append(tabIndent("r := mux.NewRouter()\n"))
+    if (endpoints.contains(Endpoint.All)) {
+      sb.append(tabIndent("// Mux directs to first matching route, i.e. the order matters\n"))
+      sb.append(
+        tabIndent(s"""r.HandleFunc("/${serviceName}/all", ${serviceName}ListHandler).Methods(http.MethodGet)""" + "\n"),
+      )
+    }
+    if (endpoints.contains(Endpoint.Create)) {
+      sb.append(
+        tabIndent(s"""r.HandleFunc("/${serviceName}", ${serviceName}CreateHandler).Methods(http.MethodPost)""" + "\n"),
+      )
+    }
+    if (endpoints.contains(Endpoint.Read)) {
+      sb.append(
+        tabIndent(
+          s"""r.HandleFunc("/${serviceName}/{id}", ${serviceName}ReadHandler).Methods(http.MethodGet)""" + "\n",
+        ),
+      )
+    }
+    if (endpoints.contains(Endpoint.Update)) {
+      sb.append(
+        tabIndent(
+          s"""r.HandleFunc("/${serviceName}/{id}", ${serviceName}UpdateHandler).Methods(http.MethodPut)""" + "\n",
+        ),
+      )
+    }
+    if (endpoints.contains(Endpoint.Delete)) {
+      sb.append(
+        tabIndent(
+          s"""r.HandleFunc("/${serviceName}/{id}", ${serviceName}DeleteHandler).Methods(http.MethodDelete)""" + "\n",
+        ),
+      )
+    }
+    sb.append(tabIndent("r.Use(jsonMiddleware)\n"))
+    sb.append("\n")
+    sb.append(tabIndent(s"""log.Fatal(http.ListenAndServe(":${port}", r))""" + "\n"))
+    sb.append("}\n\n")
+
+    sb.toString
+  }
+
   override def generate(serviceRoot: ServiceRoot): Map[File, FileContent] = {
     val usesComms = serviceRoot.comms.nonEmpty
     val serviceString = generatePackage("main") + generateImports(
         serviceRoot.name,
         serviceRoot.module,
         usesComms,
+      ) + generateMain(
+        serviceRoot.name,
+        usesComms,
+        serviceRoot.endpoints,
+        serviceRoot.port,
       )
     Map(File(serviceRoot.name, s"${serviceRoot.name}.go") -> serviceString)
     /*
--- src/test/scala/temple/generate/language/service/go/GoServiceGeneratorTestData.scala
+++ src/test/scala/temple/generate/language/service/go/GoServiceGeneratorTestData.scala
@@ -5,14 +5,26 @@ import temple.utils.FileUtils._
 
 object GoServiceGeneratorTestData {
 
-  val simpleServiceRoot: ServiceRoot = ServiceRoot("user", "github.com/TempleEight/spec-golang/user", Seq.empty)
+  val simpleServiceRoot: ServiceRoot = ServiceRoot(
+    "user",
+    "github.com/TempleEight/spec-golang/user",
+    Seq.empty,
+    Seq(Endpoint.Create, Endpoint.Read, Endpoint.Update, Endpoint.Delete),
+    80,
+  )
 
   val simpleServiceFiles: Map[File, FileContent] = Map(
     File("user", "user.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/user.go"),
   )
 
   val simpleServiceRootWithComms: ServiceRoot =
-    ServiceRoot("match", "github.com/TempleEight/spec-golang/match", Seq("user"))
+    ServiceRoot(
+      "match",
+      "github.com/TempleEight/spec-golang/match",
+      Seq("user"),
+      Seq(Endpoint.All, Endpoint.Create, Endpoint.Read, Endpoint.Update, Endpoint.Delete),
+      81,
+    )
 
   val simpleServiceFilesWithComms: Map[File, FileContent] = Map(
     File("match", "match.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/match.go"),
