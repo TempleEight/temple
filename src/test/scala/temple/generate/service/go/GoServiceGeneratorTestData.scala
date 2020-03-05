dea61da07b1bca1b756235f6b165d4f6b036e202
Charlie Harding <charlie_harding@icloud.com>
2020-03-05 10:14:56 +0000

    [openapi-gen] Fix tests by updating file path strings

--- src/test/scala/temple/generate/service/go/GoServiceGeneratorTestData.scala
+++ src/test/scala/temple/generate/service/go/GoServiceGeneratorTestData.scala
@@ -15,19 +15,19 @@ object GoServiceGeneratorTestData {
   )
 
   val simpleServiceFiles: Map[File, FileContent] = Map(
-    File("user", "go.mod")  -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/user/go.mod"),
-    File("user", "user.go") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/user/user.go"),
+    File("user", "go.mod")  -> readFile("src/test/scala/temple/generate/service/go/testfiles/user/go.mod"),
+    File("user", "user.go") -> readFile("src/test/scala/temple/generate/service/go/testfiles/user/user.go"),
     File("user/dao", "errors.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/user/dao/errors.go",
+      "src/test/scala/temple/generate/service/go/testfiles/user/dao/errors.go",
     ),
     File("user/dao", "dao.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/user/dao/dao.go",
+      "src/test/scala/temple/generate/service/go/testfiles/user/dao/dao.go",
     ),
     File("user/util", "config.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/user/util/config.go",
+      "src/test/scala/temple/generate/service/go/testfiles/user/util/config.go",
     ),
     File("user/util", "util.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/user/util/util.go",
+      "src/test/scala/temple/generate/service/go/testfiles/user/util/util.go",
     ),
   )
 
@@ -41,24 +41,24 @@ object GoServiceGeneratorTestData {
     )
 
   val simpleServiceFilesWithComms: Map[File, FileContent] = Map(
-    File("match", "go.mod") -> readFile("src/test/scala/temple/generate/language/service/go/testfiles/match/go.mod"),
+    File("match", "go.mod") -> readFile("src/test/scala/temple/generate/service/go/testfiles/match/go.mod"),
     File("match", "match.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/match/match.go",
+      "src/test/scala/temple/generate/service/go/testfiles/match/match.go",
     ),
     File("match/dao", "errors.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/match/dao/errors.go",
+      "src/test/scala/temple/generate/service/go/testfiles/match/dao/errors.go",
     ),
     File("match/dao", "dao.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/match/dao/dao.go",
+      "src/test/scala/temple/generate/service/go/testfiles/match/dao/dao.go",
     ),
     File("match/util", "config.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/match/util/config.go",
+      "src/test/scala/temple/generate/service/go/testfiles/match/util/config.go",
     ),
     File("match/util", "util.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/match/util/util.go",
+      "src/test/scala/temple/generate/service/go/testfiles/match/util/util.go",
     ),
     File("match/comm", "handler.go") -> readFile(
-      "src/test/scala/temple/generate/language/service/go/testfiles/match/comm/handler.go",
+      "src/test/scala/temple/generate/service/go/testfiles/match/comm/handler.go",
     ),
   )
 }
