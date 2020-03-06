44e4499ed833a65b3ec3455f6c1e576cf402b92a
Charlie Harding <charlie_harding@icloud.com>
2020-03-06 13:02:38 +0000

    [openapi-gen] Create a MatchPartial utility to call partial functions

--- src/main/scala/temple/utils/MonadUtils.scala
+++ src/main/scala/temple/utils/MonadUtils.scala
@@ -8,4 +8,11 @@ object MonadUtils {
     /** Reduces the Either to an item of the right type, by transforming a Left item if present */
     def fromEither(f: A => B): B = either.fold(f, identity)
   }
+
+  /** Adds the method `matchPartial` to any object, to call a partial function directly on it */
+  implicit class MatchPartial[A](x: A) {
+
+    /** Pattern-match on an object with a partial function, returning an option of a result */
+    def matchPartial[B](f: PartialFunction[A, B]): Option[B] = f.lift(x)
+  }
 }
