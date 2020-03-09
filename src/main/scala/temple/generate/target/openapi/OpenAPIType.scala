662491fe6da802b0ffdc91e1bc8f671dedf2262b
Charlie Harding <charlie_harding@icloud.com>
2020-03-09 10:40:53 +0000

    [OpenAPI-gen] Pass typeName as string

--- src/main/scala/temple/generate/target/openapi/OpenAPIType.scala
+++ src/main/scala/temple/generate/target/openapi/OpenAPIType.scala
@@ -5,8 +5,8 @@ import io.circe.syntax._
 
 import scala.collection.immutable.ListMap
 
-sealed abstract private[openapi] class OpenAPIType(customFields: Seq[(String, Json)]) extends JsonEncodable {
-  def typeString: String
+sealed abstract private[openapi] class OpenAPIType(val typeString: String, customFields: Seq[(String, Json)])
+    extends JsonEncodable {
 
   override def toJsonMap: Map[String, Option[Json]] =
     ListMap("type" -> Some(typeString.asJson)) ++ customFields.to(ListMap).view.mapValues(Some(_))
@@ -14,12 +14,12 @@ sealed abstract private[openapi] class OpenAPIType(customFields: Seq[(String, Js
 
 private[openapi] object OpenAPIType {
 
-  sealed case class OpenAPISimpleType(typeString: String, customFields: (String, Json)*)
-      extends OpenAPIType(customFields)
+  sealed case class OpenAPISimpleType(override val typeString: String, customFields: (String, Json)*)
+      extends OpenAPIType(typeString, customFields)
 
   case class OpenAPIObject(properties: Map[String, OpenAPIType], customFields: (String, Json)*)
-      extends OpenAPIType(("properties", properties.asJson) +: customFields) { val typeString = "object" }
+      extends OpenAPIType("object", ("properties", properties.asJson) +: customFields)
 
   case class OpenAPIArray(items: OpenAPIType, customFields: (String, Json)*)
-      extends OpenAPIType(("items", items.asJson) +: customFields) { val typeString = "array" }
+      extends OpenAPIType("array", ("items", items.asJson) +: customFields)
 }
