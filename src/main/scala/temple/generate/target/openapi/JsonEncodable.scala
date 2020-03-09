085dd9f7e666a4bc33edd0fa0e0c55c114c2ec32
Charlie Harding <charlie_harding@icloud.com>
2020-03-09 11:33:56 +0000

    [OpenAPI-gen] Reformat JsonEncodable

--- src/main/scala/temple/generate/target/openapi/JsonEncodable.scala
+++ src/main/scala/temple/generate/target/openapi/JsonEncodable.scala
@@ -14,16 +14,15 @@ private[openapi] trait JsonEncodable {
 private[openapi] object JsonEncodable {
 
   /** Create an encoder for JSON objects by providing a function to map them to options of values */
-  private def mapSequenceEncoder[T](toJsonMap: T => Map[String, Option[Json]]): Encoder[T] =
-    (obj: T) => {
-      val jsonMap = toJsonMap(obj)
+  private def mapSequenceEncoder[T](toJsonMap: T => Map[String, Option[Json]]): Encoder[T] = (obj: T) => {
+    val jsonMap = toJsonMap(obj)
 
-      // collect only the entries that are present
-      val somes = jsonMap.iterator.collect { case (str, Some(json)) => (str, json) }.toSeq
+    // collect only the entries that are present
+    val somes = jsonMap.iterator.collect { case (str, Some(json)) => (str, json) }.toSeq
 
-      // construct a JSON object from them
-      Json.obj(somes: _*)
-    }
+    // construct a JSON object from them
+    Json.obj(somes: _*)
+  }
 
   implicit def encodeToJson[T <: JsonEncodable]: Encoder[T] = mapSequenceEncoder(_.toJsonMap)
 }
