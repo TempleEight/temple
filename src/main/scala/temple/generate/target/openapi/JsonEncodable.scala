e31b9c2d897321597475405685ee3878e21c0b53
Charlie Harding <charlie_harding@icloud.com>
2020-03-10 14:24:03 +0000

    [openapi-gen] Recomment the JsonEncodable trait

--- src/main/scala/temple/generate/target/openapi/JsonEncodable.scala
+++ src/main/scala/temple/generate/target/openapi/JsonEncodable.scala
@@ -4,7 +4,7 @@ import io.circe.{Encoder, Json}
 
 private[openapi] trait JsonEncodable {
 
-  /** Turn a case class into a map in preparation for conversion to a JSON object */
+  /** Turn a case class into some key-value pairs in preparation for conversion to a JSON object */
   def jsonEntryIterator: IterableOnce[(String, Json)]
 
   // Required so that nested JsonEncodable interfaces always call the correct nested version
@@ -13,13 +13,15 @@ private[openapi] trait JsonEncodable {
 
 private[openapi] object JsonEncodable {
 
-  /** Create an encoder for JSON objects by providing a function to map them to options of values */
+  /** Create an encoder for JSON objects by providing a function to map them to key-value pairs */
   private def mapSequenceEncoder[T](toJsonMap: T => IterableOnce[(String, Json)]): Encoder[T] = (obj: T) => {
     Json.obj(toJsonMap(obj).iterator.toSeq: _*)
   }
 
   implicit def encodeToJson[T <: JsonEncodable]: Encoder[T] = mapSequenceEncoder(_.jsonEntryIterator)
 
+  /** Like [[temple.generate.target.openapi.JsonEncodable]] but by providing optional values, causing the entries not
+    * to render */
   trait Partial extends JsonEncodable {
     def jsonOptionEntryIterator: IterableOnce[(String, Option[Json])]
 
