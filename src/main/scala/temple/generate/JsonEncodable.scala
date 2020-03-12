178745c0299d9e6da0429dca33332105facd2765
Charlie Harding <charlie_harding@icloud.com>
2020-03-12 22:03:57 +0000

    [api] Add explicit JsonEncodable.Auto
    Note this requires adding the following definition of exportEncoder to
    any class extending Auto. Would love to know how to automate this more.
    
    N.b. Replace MyCaseClass and arguments with your own class. If this is
    used as part of an enum (trait and case classes), the trait should
    extend JsonEncodable, and the case classes should extend
    JsonEncodable.Auto/JsonEncodable.Object/JsonEncodable.Partial
    
        final case class MyCaseClass(a:A, b: B) extends JsonEncodable.Auto {
          protected def encoder: ExportedEncoder[this.type] = exportEncoder[MyCaseClass]
        }

--- src/main/scala/temple/generate/JsonEncodable.scala
+++ src/main/scala/temple/generate/JsonEncodable.scala
@@ -1,27 +1,78 @@
 package temple.generate
 
+import io.circe.export.Exported
+import io.circe.generic.encoding.DerivedAsObjectEncoder
+import io.circe.generic.util.macros.ExportMacros
+import io.circe.syntax._
 import io.circe.{Encoder, Json}
 
-private[generate] trait JsonEncodable {
+import scala.language.existentials
+import scala.language.experimental.macros
 
-  /** Turn a case class into some key-value pairs in preparation for conversion to a JSON object */
-  def jsonEntryIterator: IterableOnce[(String, Json)]
+/** Any class that can be encoded to JSON, at least partially using custom functions
+  *
+  * This trait should be implemented by any root traits, then the subtraits [[temple.generate.JsonEncodable.Auto]],
+  * [[temple.generate.JsonEncodable.Object]] and [[temple.generate.JsonEncodable.Partial]] should be implemented by any
+  * concrete implementations.
+  */
+private[generate] trait JsonEncodable {
+  protected def toJson: Json
 
   // Required so that nested JsonEncodable interfaces always call the correct nested version
-  implicit final protected def encodeToJson[T <: JsonEncodable]: Encoder[T] = JsonEncodable.encodeToJson
+  implicit final protected def jsonEncoder[T <: JsonEncodable]: Encoder[T] = a => a.toJson
 }
 
 private[generate] object JsonEncodable {
 
-  /** Create an encoder for JSON objects by providing a function to map them to key-value pairs */
-  private def mapSequenceEncoder[T](toJsonMap: T => IterableOnce[(String, Json)]): Encoder[T] = (obj: T) => {
-    Json.obj(toJsonMap(obj).iterator.toSeq: _*)
+  implicit def encodeToJson[T <: JsonEncodable]: Encoder[T] = _.toJson
+
+  /**
+    * Implement JsonEncodable as automatically as possible
+    *
+    * See the example for the full contents needed to be implemented for a case class called `MyCaseClass`.
+    *
+    * @example {{{
+    *   protected def encoder: ExportedEncoder[this.type] = exportEncoder[MyCaseClass]
+    * }}}
+    * */
+  trait Auto extends JsonEncodable {
+
+    /**
+      * The type of any encoder of a supertype of S
+      *
+      * If you can encode a supertype of S, you can also encode S, with the help of liftEncoder
+      */
+    protected type ExportedEncoder[-S] = Exported[Encoder.AsObject[C]] forSome { type C >: S }
+
+    /** Use a less specific encoder as a more specific encoder */
+    final protected def liftEncoder(encoder: ExportedEncoder[this.type]): Exported[Encoder.AsObject[this.type]] =
+      Exported(encoder.instance.contramapObject(x => x))
+
+    /** Copied from [[io.circe.generic.auto.exportEncoder]], as macros can’t be indirected */
+    protected final def exportEncoder[A]: Exported[Encoder.AsObject[A]] =
+      macro ExportMacros.exportEncoder[DerivedAsObjectEncoder, A]
+
+    /** This needs to be given in order to generate an instance, defined as `exportEncoder[MyClass]`, where MyClass is
+      * the class extending JsonEncodable.Auto */
+    protected def encoder: ExportedEncoder[this.type]
+
+    final override protected def toJson: Json =
+      EncoderOps[this.type](this).asJson(Encoder.importedEncoder(liftEncoder(encoder)))
   }
 
-  implicit def encodeToJson[T <: JsonEncodable]: Encoder[T] = mapSequenceEncoder(_.jsonEntryIterator)
+  /** Implement JsonEncodable by providing a map/key-value-sequence to go in the Json map */
+  trait Object extends JsonEncodable {
 
-  /** Like [[temple.generate.JsonEncodable]] but by providing optional values, causing the entries not to render */
-  trait Partial extends JsonEncodable {
+    /** Turn a case class into some key-value pairs in preparation for conversion to a JSON object */
+    def jsonEntryIterator: IterableOnce[(String, Json)]
+
+    // Required so that nested JsonEncodable interfaces always call the correct nested version
+    implicit final protected def toJson: Json = Json.obj(jsonEntryIterator.iterator.toSeq: _*)
+  }
+
+  /** Like [[temple.generate.JsonEncodable.Object]] but by providing optional values, causing the entries not to render
+    * if None is given */
+  trait Partial extends Object {
     def jsonOptionEntryIterator: IterableOnce[(String, Option[Json])]
 
     final override def jsonEntryIterator: IterableOnce[(String, Json)] =
