a47644142c6db0a2b7b58742d7f03bac7a19f484
Charlie Harding <charlie_harding@icloud.com>
2020-03-13 09:57:19 +0000

    [OpenAPI-gen] Add custom JsonEncodable to Handler

--- src/main/scala/temple/generate/target/openapi/Handler.scala
+++ src/main/scala/temple/generate/target/openapi/Handler.scala
@@ -1,5 +1,12 @@
 package temple.generate.target.openapi
 
+import io.circe.Json
+import io.circe.syntax._
+import io.circe.generic.auto._
+import temple.generate.JsonEncodable
+
+import scala.Option.when
+
 // https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#operationObject
 case class Handler(
   summary: String,
@@ -7,4 +14,13 @@ case class Handler(
   tags: Seq[String] = Nil,
   requestBody: Option[RequestBody] = None,
   responses: Map[Int, Response] = Map.empty,
-)
+) extends JsonEncodable.Partial {
+
+  override def jsonOptionEntryIterator: IterableOnce[(String, Option[Json])] = Seq(
+    "summary"     -> Some(summary.asJson),
+    "description" -> when(description.nonEmpty) { description.asJson },
+    "tags"        -> when(tags.nonEmpty) { tags.asJson },
+    "requestBody" -> when(requestBody.nonEmpty) { requestBody.asJson },
+    "responses"   -> when(responses.nonEmpty) { responses.asJson },
+  )
+}
