a2373298240352cd280eb07831264acd3f60250d
Charlie Harding <charlie_harding@icloud.com>
2020-03-16 14:13:19 +0000

    [Scalafmt] Revert Optimise Imports on Go code

--- src/test/scala/temple/generate/service/go/testfiles/auth/auth.go
+++ src/test/scala/temple/generate/service/go/testfiles/auth/auth.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/TempleEight/spec-golang/auth/comm"
 	"github.com/TempleEight/spec-golang/auth/dao"
+	"github.com/TempleEight/spec-golang/auth/util"
+	valid "github.com/asaskevich/govalidator"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
