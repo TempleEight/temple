8bd1d5d2973c88ed4bc9b683a1b74615c2b886ea
Will JV Smith <smithwjv@gmail.com>
2020-03-16 15:42:56 +0000

    [go-gen] Remove unnecessary format string

--- src/main/resources/go/genFiles/auth/comm/create_jwt_credential.go
+++ src/main/resources/go/genFiles/auth/comm/create_jwt_credential.go
@@ -56,7 +56,7 @@ func requestCredential(hostname string, consumer *consumerResponse) (*JWTCredent
 func (comm *Handler) CreateJWTCredential() (*JWTCredential, error) {
 	hostname, ok := comm.Services["kong-admin"]
 	if !ok {
-		return nil, fmt.Errorf("service %s's hostname not in config file", "kong-admin")
+		return nil, fmt.Errorf("service kong-admin's hostname not in config file")
 	}
 
 	// Create a consumer
