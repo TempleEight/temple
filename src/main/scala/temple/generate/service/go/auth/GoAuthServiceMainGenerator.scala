f5a9e0a84f3f3401ba13c2dcff876ed7d4baa013
Will JV Smith <smithwjv@gmail.com>
2020-03-17 15:18:29 +0000

    [go-gen] Properly reorder genFiles

--- src/main/scala/temple/generate/service/go/auth/GoAuthServiceMainGenerator.scala
+++ src/main/scala/temple/generate/service/go/auth/GoAuthServiceMainGenerator.scala
@@ -25,10 +25,10 @@ object GoAuthServiceMainGenerator {
   }
 
   private[auth] def generateStructs(): String =
-    FileUtils.readResources("go/genFiles/auth/structs.go.snippet").stripLineEnd
+    FileUtils.readResources("go/genFiles/auth/main/structs.go.snippet").stripLineEnd
 
   private[auth] def generateRouter(): String =
-    FileUtils.readResources("go/genFiles/auth/router.go.snippet").stripLineEnd
+    FileUtils.readResources("go/genFiles/auth/main/router.go.snippet").stripLineEnd
 
   private[auth] def generateMain(): String =
     mkCode(
@@ -41,8 +41,8 @@ object GoAuthServiceMainGenerator {
     )
 
   private[auth] def generateHandlers(): String =
-    FileUtils.readResources("go/genFiles/auth/handlers.go.snippet").stripLineEnd
+    FileUtils.readResources("go/genFiles/auth/main/handlers.go.snippet").stripLineEnd
 
   private[auth] def generateCreateToken(): String =
-    FileUtils.readResources("go/genFiles/auth/create_token.go.snippet").stripLineEnd
+    FileUtils.readResources("go/genFiles/auth/main/create_token.go.snippet").stripLineEnd
 }
--- src/main/scala/temple/generate/service/go/common/GoCommonMainGenerator.scala
+++ src/main/scala/temple/generate/service/go/common/GoCommonMainGenerator.scala
@@ -5,5 +5,5 @@ import temple.utils.FileUtils
 object GoCommonMainGenerator {
 
   private[go] def generateJsonMiddleware(): String =
-    FileUtils.readResources("go/genFiles/json_middleware.go.snippet").stripLineEnd
+    FileUtils.readResources("go/genFiles/common/main/json_middleware.go.snippet").stripLineEnd
 }
