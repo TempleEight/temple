298eccea3e8a7f9ae8c26ae0d53e11b1237dc993
Charlie Harding <charlie_harding@icloud.com>
2020-03-18 17:23:47 +0000

    [semantics] Remove unused import statement

--- src/main/scala/temple/DSL/semantics/SemanticParsingException.scala
+++ src/main/scala/temple/DSL/semantics/SemanticParsingException.scala
@@ -1,7 +1,5 @@
 package temple.DSL.semantics
 
-import temple.ast.Templefile
-
 /** An exception in converting the AST into a [[temple.ast.Templefile]] */
 // TODO: include contextual information
 class SemanticParsingException(str: String) extends Exception(str)
