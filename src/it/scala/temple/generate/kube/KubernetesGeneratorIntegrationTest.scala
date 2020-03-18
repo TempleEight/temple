0d1f725ff5d28604f61e25bc47966b9e441956d0
Lewis Bell <lewisbell999@gmail.com>
2020-03-18 11:46:53 +0000

    [k8s-gen] Add KubeSpec test

--- src/it/scala/temple/generate/kube/KubernetesGeneratorIntegrationTest.scala
+++ src/it/scala/temple/generate/kube/KubernetesGeneratorIntegrationTest.scala
@@ -6,6 +6,21 @@ import temple.generate.FileSystem.File
 
 class KubernetesGeneratorIntegrationTest extends KubeSpec with Matchers with BeforeAndAfter {
 
+  behavior of "KubeSpec"
+
+  it should "fail on an invalid docker spec" in {
+    val results = validateAll(
+      Map(
+        File("test", "test.yaml") -> "asdf",
+      ),
+      File("test", ""),
+    ).split('\n')
+
+    results foreach { result =>
+      result should startWith("ERR")
+    }
+  }
+
   behavior of "KubernetesGeneratorIntegrationTest"
 
   it should "generate valid simple services" in {
