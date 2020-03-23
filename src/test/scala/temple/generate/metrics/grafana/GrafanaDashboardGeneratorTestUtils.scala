f8317269793ae660fe98463c1220b8f307acf4f6
Jay Lees <jay.lees@me.com>
2020-03-23 10:00:05 +0000

    [grafana] Fix formatting of test utils

--- src/test/scala/temple/generate/metrics/grafana/GrafanaDashboardGeneratorTestUtils.scala
+++ src/test/scala/temple/generate/metrics/grafana/GrafanaDashboardGeneratorTestUtils.scala
@@ -4,14 +4,14 @@ import temple.utils.StringUtils
 
 object GrafanaDashboardGeneratorTestUtils {
 
-  private def makePanels(panels: String*): String =
+  private def makePanels(panels: Seq[String]): String =
     panels.map(panel => StringUtils.indent(panel, 4)).mkString(",")
 
-  private def makeTargets(targets: String*): String =
+  private def makeTargets(targets: Seq[String]): String =
     targets.map(target => StringUtils.indent(target, 4)).mkString(",")
 
   def makeTarget(expr: String, legend: String, refId: String): String =
-    s"""|
+    s"""
        |{
        |  "expr" : "$expr",
        |  "legendFormat" : "$legend",
@@ -29,102 +29,102 @@ object GrafanaDashboardGeneratorTestUtils {
     yAxisLabel: String,
     targets: String*,
   ): String = {
-    val rawTargets = makeTargets(targets: _*)
-    s"""|
+    val rawTargets = makeTargets(targets)
+    s"""
        |{
-        |  "aliasColors" : {
-        |        
-        |  },
-        |  "bars" : false,
-        |  "dashLength" : 10,
-        |  "dashes" : false,
-        |  "datasource" : "Prometheus",
-        |  "fill" : 1,
-        |  "fillGradient" : 0,
-        |  "gridPos" : {
-        |    "h" : $height,
-        |    "w" : $width,
-        |    "x" : $x,
-        |    "y" : $y
-        |  },
-        |  "hiddenSeries" : false,
-        |  "id" : $id,
-        |  "legend" : {
-        |    "show" : true,
-        |    "avg" : false,
-        |    "min" : false,
-        |    "rightSide" : true,
-        |    "total" : false,
-        |    "max" : false,
-        |    "values" : false,
-        |    "current" : false
-        |  },
-        |  "lines" : true,
-        |  "lineWidth" : 1,
-        |  "nullPointMode" : "null",
-        |  "options" : {
-        |    "dataLinks" : [
-        |    ]
-        |  },
-        |  "percentage" : false,
-        |  "pointradius" : 2,
-        |  "points" : false,
-        |  "renderer" : "flot",
-        |  "seriesOverrides" : [
-        |  ],
-        |  "spaceLength" : 10,
-        |  "stack" : false,
-        |  "steppedLine" : false,
-        |  "targets" : [$rawTargets
-        |  ],
-        |  "thresholds" : [
-        |  ],
-        |  "timeFrom" : null,
-        |  "timeRegions" : [
-        |  ],
-        |  "timeShift" : null,
-        |  "title" : "$title",
-        |  "tooltip" : {
-        |    "shared" : true,
-        |    "sort" : 0,
-        |    "value_type" : "individual"
-        |  },
-        |  "type" : "graph",
-        |  "x-axis" : {
-        |    "name" : null,
-        |    "show" : true,
-        |    "buckets" : null,
-        |    "mode" : "Time",
-        |    "values" : [
-        |    ]
-        |  },
-        |  "yaxes" : [
-        |    {
-        |      "format" : "short",
-        |      "show" : true,
-        |      "logBase" : 1,
-        |      "label" : "$yAxisLabel",
-        |      "min" : null,
-        |      "max" : null
-        |    },
-        |    {
-        |      "format" : "short",
-        |      "show" : true,
-        |      "logBase" : 1,
-        |      "label" : null,
-        |      "min" : null,
-        |      "max" : null
-        |    }
-        |  ],
-        |  "yaxis" : {
-        |    "align" : false,
-        |    "alignLevel" : null
-        |  }
-        |}""".stripMargin
+       |  "aliasColors" : {
+       |        
+       |  },
+       |  "bars" : false,
+       |  "dashLength" : 10,
+       |  "dashes" : false,
+       |  "datasource" : "Prometheus",
+       |  "fill" : 1,
+       |  "fillGradient" : 0,
+       |  "gridPos" : {
+       |    "h" : $height,
+       |    "w" : $width,
+       |    "x" : $x,
+       |    "y" : $y
+       |  },
+       |  "hiddenSeries" : false,
+       |  "id" : $id,
+       |  "legend" : {
+       |    "show" : true,
+       |    "avg" : false,
+       |    "min" : false,
+       |    "rightSide" : true,
+       |    "total" : false,
+       |    "max" : false,
+       |    "values" : false,
+       |    "current" : false
+       |  },
+       |  "lines" : true,
+       |  "lineWidth" : 1,
+       |  "nullPointMode" : "null",
+       |  "options" : {
+       |    "dataLinks" : [
+       |    ]
+       |  },
+       |  "percentage" : false,
+       |  "pointradius" : 2,
+       |  "points" : false,
+       |  "renderer" : "flot",
+       |  "seriesOverrides" : [
+       |  ],
+       |  "spaceLength" : 10,
+       |  "stack" : false,
+       |  "steppedLine" : false,
+       |  "targets" : [$rawTargets
+       |  ],
+       |  "thresholds" : [
+       |  ],
+       |  "timeFrom" : null,
+       |  "timeRegions" : [
+       |  ],
+       |  "timeShift" : null,
+       |  "title" : "$title",
+       |  "tooltip" : {
+       |    "shared" : true,
+       |    "sort" : 0,
+       |    "value_type" : "individual"
+       |  },
+       |  "type" : "graph",
+       |  "x-axis" : {
+       |    "name" : null,
+       |    "show" : true,
+       |    "buckets" : null,
+       |    "mode" : "Time",
+       |    "values" : [
+       |    ]
+       |  },
+       |  "yaxes" : [
+       |    {
+       |      "format" : "short",
+       |      "show" : true,
+       |      "logBase" : 1,
+       |      "label" : "$yAxisLabel",
+       |      "min" : null,
+       |      "max" : null
+       |    },
+       |    {
+       |      "format" : "short",
+       |      "show" : true,
+       |      "logBase" : 1,
+       |      "label" : null,
+       |      "min" : null,
+       |      "max" : null
+       |    }
+       |  ],
+       |  "yaxis" : {
+       |    "align" : false,
+       |    "alignLevel" : null
+       |  }
+       |}""".stripMargin
   }
 
   def makeDashboard(uid: String, title: String, panels: String*): String = {
-    val rawPanels = makePanels(panels: _*)
+    val rawPanels = makePanels(panels)
     s"""|{
         |  "annotations" : {
         |    "list" : [
