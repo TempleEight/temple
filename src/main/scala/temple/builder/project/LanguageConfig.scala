364fb0e88b89fe0c5ce2d159357f590d440bf69c
Lewis Bell <lewisbell999@gmail.com>
2020-03-30 13:27:38 +0100

    [Go-Map] Add comment explaining LanguageConfig

--- src/main/scala/temple/builder/project/LanguageConfig.scala
+++ src/main/scala/temple/builder/project/LanguageConfig.scala
@@ -2,8 +2,12 @@ package temple.builder.project
 
 import temple.ast.AttributeType
 
-/** Defines the values used for generation in a language that do not come from the user */
-sealed trait LanguageConfig {
+/** Defines the values used for generation in a language that do not come from the user
+  * inputName the name of the variable used as input, e.g. "authID", present for improving semantics of code
+  * name the name of attribute, e.g. "createdBy"
+  * attributeType type of the attribute, e.g. UUID
+  * */
+sealed trait LangnuageConfig {
   val createdByInputName: String
   val createdByName: String
   val createdByAttributeType: AttributeType
