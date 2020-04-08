4175e1dcdf10afda0f8856e71aac133d72e08582
Jay Lees <jay.lees@me.com>
2020-04-08 22:59:11 +0100

    [docker] Fix unit tests

--- src/test/scala/temple/builder/DockerfileBuilderTest.scala
+++ src/test/scala/temple/builder/DockerfileBuilderTest.scala
@@ -1,9 +1,10 @@
 package temple.builder
 
 import org.scalatest.{FlatSpec, Matchers}
-import temple.ast.AbstractServiceBlock.AuthServiceBlock
+import temple.ast.AbstractAttribute.Attribute
+import temple.ast.AbstractServiceBlock.{AuthServiceBlock, ServiceBlock}
 import temple.ast.Metadata.{Provider, ServiceLanguage}
-import temple.ast.{ProjectBlock, Templefile}
+import temple.ast.{AttributeType, Metadata, ProjectBlock, Templefile}
 
 class DockerfileBuilderTest extends FlatSpec with Matchers {
 
@@ -56,13 +57,47 @@ class DockerfileBuilderTest extends FlatSpec with Matchers {
   }
 
   it should "generate the correct Dockerfile for a Go auth service when using DockerCompose" in {
+    val templefile = Templefile(
+      "ExampleProject",
+      services = Map(
+        "AuthyService" -> ServiceBlock(
+          attributes = Map("test" -> Attribute(AttributeType.StringType())),
+          metadata = Seq(Metadata.ServiceAuth.Email),
+          structs = Map(),
+        ),
+      ),
+    )
+
+    val authBlock = templefile.allServices
+      .collectFirst {
+        case (_, AuthServiceBlock) => AuthServiceBlock
+      }
+      .getOrElse(throw new RuntimeException("Auth block doesn't exist"))
+
     val dockerfile =
-      DockerfileBuilder.createServiceDockerfile("auth", AuthServiceBlock, 80, Some(Provider.DockerCompose))
+      DockerfileBuilder.createServiceDockerfile("auth", authBlock, 80, Some(Provider.DockerCompose))
     dockerfile shouldBe DockerfileBuilderTestData.sampleAuthServiceDockerComposeDockerfile
   }
 
   it should "generate the correct Dockerfile for a Go auth service when using Kubernetes" in {
-    val dockerfile = DockerfileBuilder.createServiceDockerfile("auth", AuthServiceBlock, 80, Some(Provider.Kubernetes))
+    val templefile = Templefile(
+      "ExampleProject",
+      services = Map(
+        "AuthyService" -> ServiceBlock(
+          attributes = Map("test" -> Attribute(AttributeType.StringType())),
+          metadata = Seq(Metadata.ServiceAuth.Email),
+          structs = Map(),
+        ),
+      ),
+    )
+
+    val authBlock = templefile.allServices
+      .collectFirst {
+        case (_, AuthServiceBlock) => AuthServiceBlock
+      }
+      .getOrElse(throw new RuntimeException("Auth block doesn't exist"))
+
+    val dockerfile = DockerfileBuilder.createServiceDockerfile("auth", authBlock, 80, Some(Provider.Kubernetes))
     dockerfile shouldBe DockerfileBuilderTestData.sampleAuthServiceKubernetesDockerfile
   }
 }
