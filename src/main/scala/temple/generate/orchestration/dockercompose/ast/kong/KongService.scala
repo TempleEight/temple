076a9d8e45392d0e339cd49bbdc3b475cb882822
Jay Lees <jay.lees@me.com>
2020-04-08 23:16:06 +0100

    [docker-compose] Remove quotes from admin listen address

--- src/main/scala/temple/generate/orchestration/dockercompose/ast/kong/KongService.scala
+++ src/main/scala/temple/generate/orchestration/dockercompose/ast/kong/KongService.scala
@@ -17,7 +17,7 @@ case object KongService extends Service(Seq(8000, 8001, 8443, 8444), Seq("kong-n
         "KONG_ADMIN_ERROR_LOG=/dev/stderr",
         "KONG_PROXY_ACCESS_LOG=/dev/stdout",
         "KONG_PROXY_ERROR_LOG=/dev/stderr",
-        "KONG_ADMIN_LISTEN='0.0.0.0:8001'",
+        "KONG_ADMIN_LISTEN=0.0.0.0:8001",
         "KONG_CASSANDRA_CONTACT_POINTS=kong-db",
         "KONG_DATABASE=postgres",
         "KONG_PG_DATABASE=kong",
