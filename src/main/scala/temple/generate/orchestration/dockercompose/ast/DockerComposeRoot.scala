db853b065a04b105255b298f88a3a8fa59055bb9
Jay Lees <jay.lees@me.com>
2020-04-08 18:36:27 +0100

    [docker-compose] Add version key

--- src/main/scala/temple/generate/orchestration/dockercompose/ast/DockerComposeRoot.scala
+++ src/main/scala/temple/generate/orchestration/dockercompose/ast/DockerComposeRoot.scala
@@ -6,6 +6,7 @@ import temple.generate.JsonEncodable
 case class DockerComposeRoot(services: Map[String, Service], networks: Seq[String]) extends JsonEncodable.Object {
 
   override def jsonEntryIterator: IterableOnce[(String, Json)] = Seq(
+    "version"  ~> '3',
     "services" ~> services,
     "networks" ~> networks,
   )
