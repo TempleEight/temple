ba1d76514f408bfcb06263daefa2cd4ad2f433eb
Jay Lees <jay.lees@me.com>
2020-04-13 14:27:53 +0100

    [sql-gen] Snake case user provided attributes

--- src/e2e/resources/simple-temple-expected/simple-temple-test-user-db/init.sql
+++ src/e2e/resources/simple-temple-expected/simple-temple-test-user-db/init.sql
@@ -1,15 +1,15 @@
 CREATE TABLE simple_temple_test_user (
   id UUID NOT NULL PRIMARY KEY,
-  simpleTempleTestUser TEXT NOT NULL,
+  simple_temple_test_user TEXT NOT NULL,
   email VARCHAR(40) CHECK (length(email) >= 5) NOT NULL,
-  firstName TEXT NOT NULL,
-  lastName TEXT NOT NULL,
-  createdAt TIMESTAMPTZ NOT NULL,
-  numberOfDogs INT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  number_of_dogs INT NOT NULL,
   yeets BOOLEAN UNIQUE NOT NULL,
-  currentBankBalance REAL CHECK (currentBankBalance >= 0.0) NOT NULL,
-  birthDate DATE NOT NULL,
-  breakfastTime TIME NOT NULL
+  current_bank_balance REAL CHECK (current_bank_balance >= 0.0) NOT NULL,
+  birth_date DATE NOT NULL,
+  breakfast_time TIME NOT NULL
 );
 
 CREATE TABLE fred (
