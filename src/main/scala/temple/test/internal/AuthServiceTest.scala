5e5e280f5fb66c710555ce49a33c21c6982c9089
Jay Lees <jay.lees@me.com>
2020-04-19 15:47:44 +0100

    [project-test] Refactor to extract common logic

--- src/main/scala/temple/test/internal/AuthServiceTest.scala
+++ src/main/scala/temple/test/internal/AuthServiceTest.scala
@@ -3,41 +3,37 @@ package temple.test.internal
 import io.circe.syntax._
 import temple.ast.Metadata.AuthMethod
 import temple.utils.StringUtils
+import temple.test.internal.ServiceTestUtils._
 
-object AuthServiceTest extends ServiceTest("Auth") {
+class AuthServiceTest(baseURL: String) extends ServiceTest("Auth", baseURL, false) {
 
-  private def testEmailAuth(baseURL: String): Unit = {
-    val email    = ServiceTestUtils.randomEmail()
-    val password = StringUtils.randomString(10)
+  private def testEmailAuth(): Unit = {
+    val email       = ServiceTestUtils.randomEmail()
+    val password    = StringUtils.randomString(10)
+    val requestBody = Map("email" -> email, "password" -> password).asJson
 
     // Register endpoint
-    testEndpoint("register") { test =>
-      val registerJson = ServiceTestUtils
-        .postRequest(
-          test,
-          s"http://$baseURL/api/auth/register",
-          Map("email" -> email, "password" -> password).asJson,
-        )
+    testEndpoint("register") { (test, _) =>
+      val registerJson = postRequest(test, s"$serviceURL/register", requestBody)
       test.assert(registerJson.contains("AccessToken"), "response didn't contain the key AccessToken")
     }
 
     // Login endpoint
-    testEndpoint("login") { test =>
-      val loginJson = ServiceTestUtils
-        .postRequest(
-          test,
-          s"http://$baseURL/api/auth/login",
-          Map("email" -> email, "password" -> password).asJson,
-        )
+    testEndpoint("login") { (test, _) =>
+      val loginJson = postRequest(test, s"$serviceURL/login", requestBody)
       test.assert(loginJson.contains("AccessToken"), "response didn't contain the key AccessToken")
     }
   }
 
   // Test each type of auth that is present in the project
-  def test(authMethod: AuthMethod, baseURL: String): Boolean = {
+  def test(authMethod: AuthMethod): Boolean = {
     authMethod match {
-      case AuthMethod.Email => testEmailAuth(baseURL)
+      case AuthMethod.Email => testEmailAuth()
     }
     anyTestFailed
   }
+}
+
+object AuthServiceTest {
+  def test(authMethod: AuthMethod, baseURL: String): Boolean = new AuthServiceTest(baseURL).test(authMethod)
 }
